b3b0ca59266a12f7e7ee3c5d17e6c06d4cec3598
zmontesd <zmontesd@gmail.com>
2013-12-15 11:10:20 -0800

    made chat messages collection

--- step0/models.js
+++ step0/models.js
@@ -5,3 +5,10 @@ var ChatMessage = Backbone.Model.extend({
     time: (new Date()).getTime()
   }
 });
+
+var ChatMessages = Backbone.Collection.extend({
+    model: ChatMessage,
+    comparator: function(model) {
+      return model.get('time');
+    }
+});
