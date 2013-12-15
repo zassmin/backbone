ce843b1366e0a6d2cc79795958fe48c04d1eff00
zmontesd <zmontesd@gmail.com>
2013-12-15 11:48:15 -0800

    add getPurified as bonus

--- step0/models.js
+++ step0/models.js
@@ -1,8 +1,20 @@
+$(document).ready(function() {
+  var chat = new ChatMessage({author: 'pamela', text: 'this is the shit!', time: (new Date()).getTime()});
+  console.log(chat.getPurified());
+});
+
 var ChatMessage = Backbone.Model.extend({
   defaults: {
     author: 'Unknown',
     text: '',
     time: (new Date()).getTime()
+  }, 
+  getPurified: function() {
+    if (/shit/.test(this.get('text'))) {
+      return this.get('text').replace(/shit/, '****')
+    } else {
+      return this.get('text');
+    }
   }
 });
 
